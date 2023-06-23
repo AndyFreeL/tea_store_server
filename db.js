5a88e7bda1d66c21f2e37b6ef75265f2b9e51999
AndyFreeL <andy00freelance@gmail.com>
2023-06-23 16:03:24 +0700

    db fix pool

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
     user:'teaman',
-    password:'LffxTMx6sI7LELp2rAcI5m7qLJwriqDb',
-    host:'dpg-cf4feqo2i3mtifl0ap60-a.frankfurt-postgres.render.com',
+    password:'nXoHa1zceaH9WBRzXsvZLktY9oIuFVVs',
+    host:'dpg-cialfgp5rnupq1orrif0-a.frankfurt-postgres.render.com',
     port:5432,
-    database:'tea_store',
+    database:'teastore',
     ssl: true
 })
 
